ce8c47475820a6b5982d8a5276c606e9b6e36801
Nguyễn Trọng Sang <33416398+ErogeMaster225@users.noreply.github.com>
2023-06-07 11:24:04 +0700

    Update scoop bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,13 +1,13 @@
 # Install Scoop package manager
 if ($null -eq (Get-Command "scoop" -ErrorAction SilentlyContinue)) {
 	Invoke-RestMethod get.scoop.sh | Invoke-Expression
-	scoop bucket add .SM 'https://github.com/okibcn/ScoopMaster'
 	if ($null -eq (Get-Command "git.exe" -ErrorAction SilentlyContinue)) {
 		scoop install git
 	}
+	scoop bucket add .SM 'https://github.com/okibcn/ScoopMaster'
 }
 if (-Not (Test-Path -Path "$HOME\WinDotfiles")) {
 	git clone https://github.com/ErogeMaster225/WinDotfiles $HOME\WinDotfiles
-    git clone https://github.com/ErogeMaster225/Nilvim $HOME\WinDotfiles\nvim
+	git clone https://github.com/ErogeMaster225/Nilvim $HOME\WinDotfiles\nvim
 }
 & $HOME\WinDotfiles\setup.ps1
